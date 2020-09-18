55a4ad94dce02fd87ca533f858f50740db1f6929
Emmanuel Padilla <manolin923@gmail.com>
2020-09-18 09:51:33 -0700

    basic orderform on screen

--- src/App.js
+++ src/App.js
@@ -1,11 +1,94 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import OrderForm from "./orderform";
+import "./App.css";
+import schema from "./schema";
+import * as yup from "yup";
+
+const initialFormValues = {
+  name: "",
+  phone: "",
+  size: "",
+  pepperoni: false,
+  sausage: false,
+  ham: false,
+  pineapple: false,
+  special: "",
+};
+
+const initialFormErrors = {
+  name: "",
+  phone: "",
+  size: "",
+};
+
+const initialDisabled = true;
+const orderList = [];
 
 const App = () => {
+  const [orders, setOrders] = useState(orderList);
+  const [formValues, setFormValues] = useState(initialFormValues);
+  const [formErrors, setFormErrors] = useState(initialFormErrors);
+  const [disabled, setDisabled] = useState(initialDisabled);
+
+  const validate = (name, value) => {
+    yup
+      .reach(schema, name)
+      .validate(value)
+      .then((valid) => {
+        setFormErrors({
+          ...formErrors,
+          [name]: "",
+        });
+      })
+      .catch((err) => {
+        setFormErrors({
+          ...formErrors,
+          [name]: err.errors[0],
+        });
+      });
+  };
+
+  const inputChange = (name, value) => {
+    console.log(name, value);
+    validate(name, value);
+    setFormValues({
+      ...formValues,
+      [name]: value,
+    });
+  };
+
+  const formSubmit = () => {
+    const newOrder = {
+      first_name: formValues.first_name.trim(),
+      last_name: formValues.last_name.trim(),
+      email: formValues.email.trim(),
+      tos: formValues.tos,
+    };
+  };
+
+  // useEffect(() => {
+  //   getOrders();
+  // }, []);
+
+  // useEffect(() => {
+  //   schema.isValid(formValues).then((valid) => {
+  //     setDisabled(!valid);
+  //   });
+  // }, [formValues]);
+
   return (
-    <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
-    </>
+    <div className="container">
+      <header>
+        <h1>Welcome to GoodyGoody Pizza</h1>
+      </header>
+      <OrderForm
+        values={formValues}
+        change={inputChange}
+        submit={formSubmit}
+        disabled={disabled}
+        errors={formErrors}
+      />
+    </div>
   );
 };
 export default App;
--- src/orderform.js
+++ src/orderform.js
@@ -9,14 +9,111 @@ export default function OrderForm(props) {
   };
 
   const onChange = (evt) => {
-      const {name, value, type, checked} = evt.target
-      const valueToUse = type === 'checkbox' ? checked : value;
-      change(name, valueToUse)
-  }
-
+    const { name, value, type, checked } = evt.target;
+    const valueToUse = type === "checkbox" ? checked : value;
+    change(name, valueToUse);
+  };
 
   return (
     <form classname="formContainer" onSubmit={onSubmit}>
+      <div className="formInputs">
+        <label>
+          {""}
+          Name
+          <input
+            value={values.name}
+            onChange={onChange}
+            name="name"
+            type="text"
+          />
+        </label>
+
+        <label>
+          {""}
+          Telephone Number
+          <input
+            value={values.phone}
+            onChange={onChange}
+            name="name"
+            type="tel"
+          />
+        </label>
+        <h2>Select a Size</h2>
+        <label>
+          {""}
+          Size
+          <select name="size" value={values.size} onChange={onChange}>
+            <option value=""> ---Select a Size---</option>
+            <option value="personal"> 6" Personal</option>
+            <option value="small"> 12" Small</option>
+            <option value="medium"> 14" Medium </option>
+            <option value="large"> 16" Large</option>
+            <option value="party"> 24" Party Box</option>
+          </select>
+        </label>
+      </div>
+
+      <div>
+        <h2>Select Toppings</h2>
+
+        <label>
+          {""}
+          toppings
+          <input
+            type="checkbox"
+            name="pepperoni"
+            checked={values.pepperoni}
+            onChange={onChange}
+          />
+        </label>
+
+        <label>
+          {""}
+          toppings
+          <input
+            type="checkbox"
+            name="sausage"
+            checked={values.sausage}
+            onChange={onChange}
+          />
+        </label>
+
+        <label>
+          {""}
+          toppings
+          <input
+            type="checkbox"
+            name="ham"
+            checked={values.ham}
+            onChange={onChange}
+          />
+        </label>
+
+        <label>
+          {""}
+          toppings
+          <input
+            type="checkbox"
+            name="pineapple"
+            checked={values.pineapple}
+            onChange={onChange}
+          />
+        </label>
+      </div>
+
+      <div className="special">
+        <label>
+          {""}
+          Special Instructions
+          <input
+            value={values.special}
+            onChange={onChange}
+            name="Special"
+            type="text"
+          />
+        </label>
+      </div>
+
       <div className="formsubmit">
         <h2>Order</h2>
         <button id="button" dissabled={disabled}>
@@ -30,60 +127,6 @@ export default function OrderForm(props) {
           <div>{errors.toppings}</div>
           <div>{errors.special}</div>
         </div>
-      </div>
-
-      <div className="formInputs">
-        <label>
-          {""}
-          Name
-          <input
-          value={values.name}
-          onChange={onChange}
-          name="name"
-          type="text"
-          />
-        </label>
-
-        <label>
-          {""}
-          Telephone Number
-          <input
-          value={values.phone}
-          onChange={onChange}
-          name="name"
-          type="tel"
-          />
-        </label>
-        <h2>Select a Size</h2>
-        <label>
-          {""}
-          Size
-          <select name='size' value={values.size} onChange={onChange}>
-            <option value= ""> ---Select a Size---</option>
-            <option value= "personal"> 6" Personal</option>
-            <option value= "small"> 12" Small</option>
-            <option value= "medium"> 14" Medium </option>
-            <option value= "large"> 16" Large</option>
-            <option value= "party"> 24" Party Box</option>
-
-            </select>
-            </label>
-            </div>
-
-            <div>
-            <h2>Select Toppings</h2>
-            
-            <label>
-                {""}
-                toppings
-                <input
-                type="checkbox"
-                name='topping'
-                checked={values.topping}
-
-
-            </label>
-
       </div>
     </form>
   );
